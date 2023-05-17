5d84ff3685d35c1a56d88f25b2a3a6728cf839d6
Jaseil <jaseillightfoot227@boyslatin.net>
2023-05-17 09:15:05 -0400

    Void

--- void.py
+++ void.py
@@ -9,66 +9,66 @@ def clanName():
     return clanName[clan_generator]
 
 def powersystem():
+    powersystem = ['stats']
     stat_points = ['vitality' , 'strength' , 'weapon']
-    stat_increase = random.range(3, 10)
+    stat_increase = random,range(3, 10)
     stat_generator = random.randint(0, 2)
 
 
-    if boss_level == 15:
-        new_stat = stat_generator('x5')
+    #if boss_level == 15:
+        #new_stat = stat_generator('x5')
 
     
-    
-    print(" you gained " + str(stat_points) + "plus point" + str (stat_increase) + "increased points" (stat_generator) + "random points")
+    print(" you gained " , str(stat_points) , "plus point" , str(stat_increase) , "increased points" , (stat_generator) , "random points")
 
 
 
 def travel():
-    miles_traveled = random.range(38,61)
-    days_spent = random.range(3,8)
+    miles_traveled = random.randrange(38,61)
+    days_spent = random.randrange(3,8)
     
     print("you've traveled " + str(miles_traveled) + "miles & it took" +str (days_spent) + " days")
 
 
-# def rest():
-#     vitality_increase: int = random.randrange(1,6)
-#     days_spent = random.randrange(2,6)
+def rest():
+    vitality_increase: int = random.randrange(1,6)
+    days_spent = random.randrange(2,6)
 
-#     print((vitality_increase) + (days_spent))
-
-
-# def hunt():
-#     animal = 100
-#     days_spent = random.randrange(2,6)
-
-#     print((animal) + (days_spent))
+    print((vitality_increase) + (days_spent))
 
 
-# def status_bag():
-#     commands = ['Travel','Rest', 'Hunt', 'Status', 'Help']
-#     print(commands)
+def hunt():
+     animal = 100
+     days_spent = random.randrange(2,6)
+
+     print((animal) + (days_spent))
+
+
+def status_bag():
+    commands = ['Travel','Rest', 'Hunt', 'Status', 'Help']
+    print(commands)
 
 # def save():
 #     save_all = datetime + random
 #     print(save_all)
-
-# clanName()
-# powersystem()
-# travel()
-# rest()
-# hunt()
-# status_bag()
+     
+clanName()
+powersystem()
+travel()
+rest()
+hunt()
+status_bag()
 # save()
 
-# print('clanName' , 'powersystem' , 'travel' , 'rest' , 'hunt' , 'status_bag')
+print('clanName' , 'powersystem' , 'travel' , 'rest' , 'hunt' , 'status_bag')
 
-# #User will get a in game note telling the user how to play the game
+ #User will get a in game note telling the user how to play the game
 
-# print('welcome to the void follow the arrow to the cave')
+print('welcome to the void follow the arrow to the cave')
 
-# def runGame():
-#     rungame = ('play')
+def runGame():
+ runGame = ('play')
     
-#     print (rungame)
+# print (runGame)
 
 print(clanName())
